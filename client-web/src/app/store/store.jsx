9f8e56b688a174003814752a8af708f723224c4a
fatiherkamdinler <fatihdinler458@gmail.com>
2023-09-24 11:28:30 +0300

    [client-web] completed sidebar of store page

--- client-web/src/app/store/store.jsx
+++ client-web/src/app/store/store.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Breadcrumb, Helmet } from '../../components'
-import { Text, Checkbox, Stack, Input, InputGroup, InputLeftAddon } from '@chakra-ui/react'
+import { Text, Checkbox, Stack, Input, InputGroup, InputLeftAddon, Image } from '@chakra-ui/react'
+import ReactStars from 'react-rating-stars-component'
 
 const Store = () => {
   return (
@@ -16,6 +17,7 @@ const Store = () => {
         <div className='container-xxl'>
           <div className='row'>
             <div className='col-3'>
+
               <div className='filter-card rounded-3 mb-3'>
                 <Text as='h3' className='filter-title'>Shop By Categories</Text>
                 <ul>
@@ -25,6 +27,7 @@ const Store = () => {
                   <li>Laptop</li>
                 </ul>
               </div>
+
               <div className='filter-card rounded-3 mb-3'>
                 <Text as='h3' className='filter-title'>Filter By</Text>
                 <div>
@@ -80,13 +83,95 @@ const Store = () => {
                       <li></li>
                     </ul>
                   </div>
+
+                  <Text as='h5' className='sub-title'>Size</Text>
+                  <Stack>
+                    <Checkbox
+                      colorScheme='teal'
+                      className='checkbox-item'
+                      size={'md'}>
+                      Small (2)
+                    </Checkbox>
+                    <Checkbox
+                      colorScheme='teal'
+                      className='checkbox-item'
+                      size={'md'}>
+                      Medium (9)
+                    </Checkbox>
+                  </Stack>
                 </div>
               </div>
+
               <div className='filter-card rounded-3 mb-3'>
                 <Text as='h3' className='filter-title'>Product Tags</Text>
+                <div className='product-tags d-flex flex-wrap align-items-center gap-10'>
+                  <span className='badge bg-light text-secondary rounded-3 py-2 px-3'>
+                    Headphone
+                  </span>
+                  <span className='badge bg-light text-secondary rounded-3 py-2 px-3'>
+                    Laptops
+                  </span>
+                  <span className='badge bg-light text-secondary rounded-3 py-2 px-3'>
+                    Mobile
+                  </span>
+                  <span className='badge bg-light text-secondary rounded-3 py-2 px-3'>
+                    Speakers
+                  </span>
+                  <span className='badge bg-light text-secondary rounded-3 py-2 px-3'>
+                    Tablets
+                  </span>
+                </div>
               </div>
+
               <div className='filter-card rounded-3 mb-3'>
                 <Text as='h3' className='filter-title'>Random Products</Text>
+                <div className='random-products d-flex'>
+                  <div className='w-25'>
+                    <Image
+                      src='images/watch.jpg'
+                      alt='random product'
+                      className='img-fluid'
+                    />
+                  </div>
+                  <div className='w-75'>
+                    <Text as='h5'>
+                      Kids watches bulk 10 packs multi colored for students
+                    </Text>
+                    <ReactStars
+                      count={5}
+                      size={24}
+                      value={3}
+                      edit={false}
+                      activeColor='teal'
+                      classNames='stars'
+                    />
+                    <Text as='p'>$299</Text>
+                  </div>
+                </div>
+
+                <div className='random-products d-flex'>
+                  <div className='w-25'>
+                    <Image
+                      src='images/watch.jpg'
+                      alt='random product'
+                      className='img-fluid'
+                    />
+                  </div>
+                  <div className='w-75'>
+                    <Text as='h5'>
+                      Kids watches bulk 10 packs multi colored for students
+                    </Text>
+                    <ReactStars
+                      count={5}
+                      size={24}
+                      value={3}
+                      edit={false}
+                      activeColor='teal'
+                      classNames='stars'
+                    />
+                    <Text as='p'>$299</Text>
+                  </div>
+                </div>
               </div>
             </div>
             <div className='col-9'>
