d7c6d9deb66d28f225ae0f6237b00721232e91db
fatiherkamdinler <fatihdinler458@gmail.com>
2023-10-05 22:41:25 +0300

    [admin: master] admin project initialization & structuring

---
+++ admin/src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Dashboard } from './dashboard/dashboard'
+export { default as ForgotPassword } from './forgot-password/forgot-password'
+export { default as Login } from './login/login'
+export { default as ResetPassword } from './reset-password/reset-password'
