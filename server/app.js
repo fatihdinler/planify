db19f61b0a3cd64b9c7d7fc4f589f2710ea0f5f8
fatiherkamdinler <fatihdinler458@gmail.com>
2023-12-15 21:19:24 +0300

    [master] added docker configurations

--- server/app.js
+++ server/app.js
@@ -1,20 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const errorHandler = require('./src/middlewares/error.middleware')
-const connectDb = require('./src/utils/db')
-const userRoutes = require('./src/routes/user.route')
 
-connectDb()
 const app = express()
 app.use(express.json())
 
-const PORT = process.env.PORT || 4001
 const baseApiUrl = `/api/v1`
+const PORT = process.env.PORT || 3001
 
-app.use(`${baseApiUrl}/users`, userRoutes)
-
-app.use(errorHandler)
+app.get('/hello', (req, res) => {
+  res.send('Hello')
+})
 
 app.listen(PORT, () => {
-  console.log(`>>> server app is running on http://localhost:${PORT}`) // Değişiklik yapıldı
+  console.log(`>>> server app is running on http://localhost:${PORT}}`) // Değişiklik yapıldı
 })
--- server/src/middlewares/error.middleware.js
+++
@@ -1,7 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  console.log(`>>> error in fn:errorHandler: ${err.stack}`)
-  res.status(500).send(err)
-  next()
-}
-
-module.exports = errorHandler
